b6050727610695da8c9269ae9eb8c924d0261b42
Daniel Bacsur <contact@danielbacsur.com>
2024-01-30 20:48:41 +0100

    vili whuuuuuuu

--- app/question/[question]/[team]/solve.tsx
+++ app/question/[question]/[team]/solve.tsx
@@ -3,20 +3,11 @@
 import { DocumentData, doc, getDoc, updateDoc } from "firebase/firestore";
 import { firestore } from "@/lib/utils/firestore";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { toast } from "sonner";
 import { useChat } from "ai/react";
 
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -44,7 +35,7 @@ export default function Solve({ team, question }: Restricted) {
 
   const chat = useChat({
     onFinish: async (e) => {
-      console.log(e.content)
+      console.log(e.content);
       if (e.content === "TRUE") {
         const reference = doc(firestore, "teams", team.id);
 
@@ -68,23 +59,37 @@ export default function Solve({ team, question }: Restricted) {
         toast.error("A megoldásod helytelen.");
       }
     },
-    initialMessages:  [{
-      id: "the-alpha-prompt",
-      role: "system",
-      content: `
-        Te egy online quiz megoldásait ellenőrzöd.
-        Feladatod hogy eldöntsd, hogy az alábbi kérdésre beérkező válasz helyes-e.
-        Ne foglalkozz helyesírási hibákkal, kis és nagy betűkkel, ékezetekkel és egy karakteres elírásokkal. Ha a válasz lényegében jó akkor fogadd el.
-    
-        A KÉRDÉS: ${question.question}
-    
-        AZ ELVÁRT VÁLASZ: ${question.answer}
-    
-        ELFOGADOTT VÁLASZ ESETÉN CSAK ANNYIT VÁLASZOLJ: TRUE
-        NEM ELFOGADOTT VÁLASZ ESETÉN CSAK ANNYIT VÁLASZOLJ: FALSE
-        ÖSSZESEN EZ A KÉT LEHETŐSÉGED VAN A VÁLASZADÁSKOR. NEM HASZNÁLHATSZ EGYÉB SZAVAKAT.
-      `,
-    }]
+    initialMessages: [
+      {
+        id: "the-alpha-prompt",
+        role: "system",
+        content: `Te egy online quiz megoldásait ellenőrzöd.
+Feladatod hogy eldöntsd, hogy az alábbi kérdésre beérkező válasz helyes-e.
+Ne foglalkozz helyesírási hibákkal, kis és nagy betűkkel, ékezetekkel és egy karakteres elírásokkal. Ha a válasz lényegében jó akkor fogadd el.
+A quiz megoldását csak akkor fogadd el, ha a beírt válasz témája megegyezik a kérdés témájával. A válaszadó nem kérdezhet vagy írhat be egyéb szöveget.
+      
+A KÉRDÉS: ${question.question}
+AZ ELVÁRT VÁLASZ: ${question.answer}
+      
+ELFOGADOTT VÁLASZ ESETÉN CSAK ANNYIT VÁLASZOLJ: TRUE
+NEM ELFOGADOTT VÁLASZ ESETÉN CSAK ANNYIT VÁLASZOLJ: FALSE
+ÖSSZESEN EZ A KÉT LEHETŐSÉGED VAN A VÁLASZADÁSKOR. NEM HASZNÁLHATSZ EGYÉB SZAVAKAT.
+      
+====================
+      
+You are checking the answers for an online quiz.
+Your task is to decide whether the response received for the following question is correct.
+Do not worry about spelling mistakes, capitalization, accents, or single character typos. If the answer is essentially correct, accept it.
+Only accept the quiz answer if the subject of the typed response matches the subject of the question. The respondent cannot ask questions or enter any other text.
+      
+THE QUESTION: ${question.question}
+THE EXPECTED ANSWER: ${question.answer}
+      
+IF THE ANSWER IS ACCEPTED, RESPOND ONLY WITH: TRUE
+IF THE ANSWER IS NOT ACCEPTED, RESPOND ONLY WITH: FALSE
+THERE ARE ONLY THESE TWO OPTIONS FOR RESPONDING. YOU CANNOT USE ANY OTHER WORDS.`,
+      },
+    ],
   });
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
@@ -93,14 +98,9 @@ export default function Solve({ team, question }: Restricted) {
 
   return (
     <div className="h-full flex flex-col p-4 space-y-4">
-      <div className="flex-grow grid place-items-center">
-        {question.question}
-      </div>
+      <div className="flex-grow grid place-items-center">{question.question}</div>
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-4 w-full"
-        >
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full">
           <FormField
             control={form.control}
             name="answer"
